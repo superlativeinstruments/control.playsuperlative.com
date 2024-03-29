047e8ff4ff19c87a3ff89ada6d8442bc32b8dad0
Rune Warhuus <rune@dinkdonk.no>
2024-03-30 00:20:10 +0100

    Fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
-	server: {
+	preview: {
 		host: 'control.playsuperlative.com',
 		// port: 433,
 		strictPort: true,
